a25290877cc0c01d333587c071948bafe86cb620
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2025-02-21 00:35:12 -0600

    Set current time and time zone with prompts for user input, add while loop and if elsif statements to get definitive answer from user

--- app.rb
+++ app.rb
@@ -2,36 +2,105 @@ require "tzinfo"
 require "date"
 
 
-# for i in "Hello, Welcome to Time Zone Converter!".chars.to_a
-#   print i
-#   sleep 0.03
-# end
+for i in "Hello, Welcome to Time Zone Converter!".chars.to_a
+  print i
+  sleep 0.03
+end
 
-# sleep(2)
+sleep(2)
 
-# puts "\n"
+puts "\n"
 
-# for i in "What is your name?".chars.to_a
-#     print i
-#     sleep 0.03
-# end
+for i in "What is your name?".chars.to_a
+    print i
+    sleep 0.03
+end
 
-# puts "\n"
+puts "\n"
 
-# name = gets.chomp
-
-# sleep(3)
-
-
-# for i in "Nice to meet you, #{name}!".chars.to_a
-#     print i
-#     sleep 0.03
-# end
+name = gets.chomp
 
 # sleep(3)
+
+
+for i in "Nice to meet you, #{name}!".chars.to_a
+    print i
+    sleep 0.03
+end
+
+sleep(1)
+
+puts "\n"
+
 
 
 time = Time.new
 
-puts "The time is currently " + time.strftime("%I:%M") + ", correct?"
+current_time = time.strftime("%I:%M")
+
+time_zone = Time.now.zone
+
+
+
+
+for i in "The time is currently #{current_time} #{time_zone} here, correct?".chars.to_a
+    print i
+    sleep 0.05
+end
+
+sleep(3)
+
+puts "\n"
+
+
+
+answered = false
+
+while answered == false
+    confirm_or_deny = gets.chomp
+    if (confirm_or_deny.upcase == "y".upcase || confirm_or_deny.upcase == "yes".upcase)
+        answered = true
+        for i in "Yes? Thank you".chars.to_a
+            print i
+            sleep 0.03
+        end
+    
+    elsif (confirm_or_deny.upcase == "n".upcase || confirm_or_deny.upcase == "no".upcase)
+        answered = true
+        for i in "No? Well that's a problem".chars.to_a
+            print i
+            sleep 0.03
+        end
+    else
+        for i in "Yes or No please".chars.to_a
+            print i
+            sleep 0.03
+        end
+        puts "\n"
+    end
+
+end
+
+
+
+
+
+
+puts "\n"
+
+
+
+
+
+
+
+
+# Plan out app
+
+# on start of program, greets and prompts for username input
+# detects current time zone, asks user to verify if accurate
+# asks what current availability is weekly, including what days of week and hours of the day they ARE available, hours of the day they ARE NOT available, maybe store in a hash?
+# ask what other time zone you want to schedule with
+# display availability in the other time zone
+
 
