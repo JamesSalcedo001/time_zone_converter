c223f3b246bddbcaa39f23d29e2180719feb90f6
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2025-02-21 01:58:21 -0600

    Build out time zone user hash table, test add values

--- app.rb
+++ app.rb
@@ -2,94 +2,135 @@ require "tzinfo"
 require "date"
 
 
-for i in "Hello, Welcome to Time Zone Converter!".chars.to_a
-  print i
-  sleep 0.03
-end
+# for i in "Hello, Welcome to Time Zone Converter!".chars.to_a
+#   print i
+#   sleep 0.03
+# end
 
-sleep(2)
+# sleep(2)
 
-puts "\n"
+# puts "\n"
 
-for i in "What is your name?".chars.to_a
-    print i
-    sleep 0.03
-end
+# for i in "What is your name?".chars.to_a
+#     print i
+#     sleep 0.03
+# end
 
-puts "\n"
+# puts "\n"
 
-name = gets.chomp
+# name = gets.chomp
+
+# # sleep(3)
+
+
+# for i in "Nice to meet you, #{name}!".chars.to_a
+#     print i
+#     sleep 0.03
+# end
+
+# sleep(1)
+
+# puts "\n"
+
+
+
+# time = Time.new
+
+# current_time = time.strftime("%I:%M")
+
+# current_time_zone = Time.now.zone
+
+
+
+
+# for i in "The time is currently #{current_time} #{current_time_zone} here, correct?".chars.to_a
+#     print i
+#     sleep 0.05
+# end
 
 # sleep(3)
 
+# puts "\n"
 
-for i in "Nice to meet you, #{name}!".chars.to_a
-    print i
-    sleep 0.03
-end
 
-sleep(1)
 
-puts "\n"
+# answered = false
+
+# while answered == false
+#     confirm_or_deny = gets.chomp
+#     if (confirm_or_deny.upcase == "y".upcase || confirm_or_deny.upcase == "yes".upcase)
+#         answered = true
+#         for i in "Yes? Thank you".chars.to_a
+#             print i
+#             sleep 0.03
+#         end
+    
+#     elsif (confirm_or_deny.upcase == "n".upcase || confirm_or_deny.upcase == "no".upcase)
+#         answered = true
+#         for i in "No? Well that's a problem".chars.to_a
+#             print i
+#             sleep 0.03
+#         end
+#     else
+#         for i in "Yes or No please".chars.to_a
+#             print i
+#             sleep 0.03
+#         end
+#         puts "\n"
+#     end
+
+# end
+
+
+
+
+
+
+# puts "\n"
+
 
 
 
 time = Time.new
 
-current_time = time.strftime("%I:%M")
 
-time_zone = Time.now.zone
+current_time_zone = Time.now.zone
 
+time_zone_user = {
+    name: nil,
+    time_zone: nil,
+    availability: {
+        monday: {
+            time_slot_1: [] || "n/a"
+        },
+        tuesday: {
+            time_slot_1: [] || "n/a"
+        },
+        wednesday: {
+            time_slot_1: [] || "n/a"
+        },
+        thursday: {
+            time_slot_1: [] || "n/a"
+        },
+        friday: {
+            time_slot_1: [] || "n/a"
+        },
+        saturday: {
+            time_slot_1: [] || "n/a"
+        },
+        sunday: {
+            time_slot_1: [] || "n/a"
+        }
+    }
+}
 
+answer = 1, 2
 
+time_zone_user[:time_zone] = current_time_zone
+# time_zone_user[:name] = gets.chomp
+time_zone_user[:availability][:monday][:time_slot_1] = answer
 
-for i in "The time is currently #{current_time} #{time_zone} here, correct?".chars.to_a
-    print i
-    sleep 0.05
-end
-
-sleep(3)
-
-puts "\n"
-
-
-
-answered = false
-
-while answered == false
-    confirm_or_deny = gets.chomp
-    if (confirm_or_deny.upcase == "y".upcase || confirm_or_deny.upcase == "yes".upcase)
-        answered = true
-        for i in "Yes? Thank you".chars.to_a
-            print i
-            sleep 0.03
-        end
-    
-    elsif (confirm_or_deny.upcase == "n".upcase || confirm_or_deny.upcase == "no".upcase)
-        answered = true
-        for i in "No? Well that's a problem".chars.to_a
-            print i
-            sleep 0.03
-        end
-    else
-        for i in "Yes or No please".chars.to_a
-            print i
-            sleep 0.03
-        end
-        puts "\n"
-    end
-
-end
-
-
-
-
-
-
-puts "\n"
-
-
-
+puts time_zone_user
 
 
 
