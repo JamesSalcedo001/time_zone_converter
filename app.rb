286fb026160694f16648db47f58be2a607314d9a
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2025-02-21 02:46:07 -0600

    Move variables to top of file, alter timing and responses for better user experience, use user prompts to gather data and insert it into hash for organization

--- app.rb
+++ app.rb
@@ -2,99 +2,15 @@ require "tzinfo"
 require "date"
 
 
-# for i in "Hello, Welcome to Time Zone Converter!".chars.to_a
-#   print i
-#   sleep 0.03
-# end
-
-# sleep(2)
-
-# puts "\n"
-
-# for i in "What is your name?".chars.to_a
-#     print i
-#     sleep 0.03
-# end
-
-# puts "\n"
-
-# name = gets.chomp
-
-# # sleep(3)
-
-
-# for i in "Nice to meet you, #{name}!".chars.to_a
-#     print i
-#     sleep 0.03
-# end
-
-# sleep(1)
-
-# puts "\n"
-
-
-
-# time = Time.new
-
-# current_time = time.strftime("%I:%M")
-
-# current_time_zone = Time.now.zone
-
-
-
-
-# for i in "The time is currently #{current_time} #{current_time_zone} here, correct?".chars.to_a
-#     print i
-#     sleep 0.05
-# end
-
-# sleep(3)
-
-# puts "\n"
-
-
-
-# answered = false
-
-# while answered == false
-#     confirm_or_deny = gets.chomp
-#     if (confirm_or_deny.upcase == "y".upcase || confirm_or_deny.upcase == "yes".upcase)
-#         answered = true
-#         for i in "Yes? Thank you".chars.to_a
-#             print i
-#             sleep 0.03
-#         end
-    
-#     elsif (confirm_or_deny.upcase == "n".upcase || confirm_or_deny.upcase == "no".upcase)
-#         answered = true
-#         for i in "No? Well that's a problem".chars.to_a
-#             print i
-#             sleep 0.03
-#         end
-#     else
-#         for i in "Yes or No please".chars.to_a
-#             print i
-#             sleep 0.03
-#         end
-#         puts "\n"
-#     end
-
-# end
-
-
-
-
-
-
-# puts "\n"
-
-
-
-
 time = Time.new
 
+current_time = time.strftime("%I:%M")
 
 current_time_zone = Time.now.zone
+
+answered = false
+
+
 
 time_zone_users = {
         time_zone_user1: {
@@ -153,15 +69,118 @@ time_zone_users = {
         }
 }
 
-answer = 1, 2
-# answer = "n/a"
-time_zone_users[:time_zone_user1][:time_zone] = current_time_zone
-# time_zone_users[:time_zone_user1][:name] = gets.chomp
-time_zone_users[:time_zone_user1][:availability][:monday][:time_slot_1] = answer
 
-puts time_zone_users[:time_zone_user1]
+
+
+for i in "Hello, Welcome to Time Zone Converter!".chars.to_a
+  print i
+  sleep 0.03
+end
+
+sleep(2)
+
 puts "\n"
-puts time_zone_users[:time_zone_user2]
+
+for i in "What is your name?".chars.to_a
+    print i
+    sleep 0.03
+end
+
+puts "\n"
+
+name = gets.chomp
+
+time_zone_users[:time_zone_user1][:name] = name
+
+for i in "Nice to meet you, #{name}!".chars.to_a
+    print i
+    sleep 0.03
+end
+
+sleep(1)
+
+puts "\n"
+
+
+
+for i in "The time is currently #{current_time} #{current_time_zone} here, correct?".chars.to_a
+    print i
+    sleep 0.05
+end
+
+
+puts "\n"
+
+
+
+
+while answered == false
+    confirm_or_deny = gets.chomp
+    if (confirm_or_deny.upcase == "y".upcase || confirm_or_deny.upcase == "yes".upcase)
+        answered = true
+        time_zone_users[:time_zone_user1][:time_zone] = current_time_zone
+        for i in "Yes? Thank you".chars.to_a
+            print i
+            sleep 0.03
+        end
+    
+    elsif (confirm_or_deny.upcase == "n".upcase || confirm_or_deny.upcase == "no".upcase)
+        answered = true
+        for i in "No? Well, that's a problem....".chars.to_a
+            print i
+            sleep 0.05
+        end
+        sleep(1)
+        puts "\n"
+        for i in "What is your current time zone?".chars.to_a
+            print i
+            sleep 0.03
+        end
+        puts "\n"
+        manual_time_zone = gets.chomp
+        time_zone_users[:time_zone_user1][:time_zone] = manual_time_zone
+        for i in "#{manual_time_zone}? Okay, thank you".chars.to_a
+            print i
+            sleep 0.03
+        end
+    else
+        for i in "Yes or No please".chars.to_a
+            print i
+            sleep 0.03
+        end
+        puts "\n"
+    end
+
+end
+
+
+
+
+
+
+puts "\n"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# answer = 1, 2
+# answer = "n/a"
+# time_zone_users[:time_zone_user1][:time_zone] = current_time_zone
+# time_zone_users[:time_zone_user1][:name] = gets.chomp
+# time_zone_users[:time_zone_user1][:availability][:monday][:time_slot_1] = answer
+
+
 
 
 
