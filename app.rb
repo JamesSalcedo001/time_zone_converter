a124b984362ad80ab5d1cfb8e8e64829fd29ef66
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2025-02-22 01:46:23 -0600

    Add shorter path to move through faster but less interactive

--- app.rb
+++ app.rb
@@ -11,7 +11,9 @@ current_time = time.strftime("%I:%M")
 
 current_time_zone = Time.now.zone
 
-answered = false
+answered_outer = false
+
+answered_inner = false
 
 
 
@@ -77,6 +79,13 @@ time_zone_users = {
 
 
 
+
+
+
+
+
+
+
 # greeting
 
 
@@ -91,88 +100,231 @@ puts "\n"
 
 
 
-# ask for user1 name
 
-for i in "What is your name?".chars.to_a
-    print i
-    sleep 0.03
+for i in "If you've already been here, would you like the short version?".chars.to_a
+  print i
+  sleep 0.03
 end
 
-puts "\n"
-
-name = gets.chomp
-
-
-
-# store user input name value in the first hash
-
-
-
-time_zone_users[:time_zone_user1][:name] = name
-
-for i in "Nice to meet you, #{name}!".chars.to_a
-    print i
-    sleep 0.03
-end
-
-sleep(1)
+sleep(2)
 
 puts "\n"
 
 
+# gives user the option to answer questions quickly with less interactivity
 
-# checks if current time and time zone is accurately detected
-
-
-
-for i in "The time is currently #{current_time} #{current_time_zone} here, correct?".chars.to_a
-    print i
-    sleep 0.05
-end
-
-
-puts "\n"
-
-
-
-
-# keeps user within the prompts until they give an answer
-
-# if they give a y or yes, uppercase or lowercase or any of the letters mixed case, 'answered' is set to true, which exits the while loop, sets the first user's current time zone to whatever the current_time_zone variable is detecting(ex: CST, MT etc), and then answers with a thank you.
-
-# else if they give a n or no, uppercase or lowercase or any of the letters mixed case, 'answered' is still set to true, but responds with asking what the current time zone is from the user. when user responds, time zone is collected manually from them and then answers with a thank you
-
-# if user does not respond with any version of y/yes, or n/no, the default response asks user to answer with a yes or a no, waits for response, and will repeat until user gives a valid response
-
-while answered == false
+while answered_outer == false
     confirm_or_deny = gets.chomp
     if (confirm_or_deny.upcase == "y".upcase || confirm_or_deny.upcase == "yes".upcase)
-        answered = true
+        answered_outer = true
+        
+        # faster path
+
+        puts "name?"
+        puts "\n"
+        fast_user1_name = gets.chomp
+        time_zone_users[:time_zone_user1][:name] = fast_user1_name
+
+        puts "time and time zone is set to: #{current_time} #{current_time_zone}"
         time_zone_users[:time_zone_user1][:time_zone] = current_time_zone
-        for i in "Yes? Thank you".chars.to_a
+        puts "\n"
+
+        puts "name2?"
+        puts "\n"
+        fast_user2_name = gets.chomp
+        time_zone_users[:time_zone_user2][:name] = fast_user2_name
+
+
+        puts "other time zone?"
+        puts "\n"
+        fast_user_2_time_zone = gets.chomp
+        time_zone_users[:time_zone_user2][:time_zone] = fast_user_2_time_zone
+
+
+        puts time_zone_users[:time_zone_user1]
+        puts "\n"
+        puts time_zone_users[:time_zone_user2]
+
+
+
+
+
+    
+    elsif (confirm_or_deny.upcase == "n".upcase || confirm_or_deny.upcase == "no".upcase)
+        answered_outer = true
+        
+
+        puts "\n"
+
+
+        # ask for user1 name
+
+        for i in "What is your name?".chars.to_a
             print i
             sleep 0.03
         end
-    
-    elsif (confirm_or_deny.upcase == "n".upcase || confirm_or_deny.upcase == "no".upcase)
-        answered = true
-        for i in "No? Well, that's a problem....".chars.to_a
+
+        puts "\n"
+
+        name = gets.chomp
+
+
+
+        # store user input name value in the first hash
+
+
+
+        time_zone_users[:time_zone_user1][:name] = name
+
+        for i in "Nice to meet you, #{name}!".chars.to_a
+            print i
+            sleep 0.03
+        end
+
+        sleep(1)
+
+        puts "\n"
+
+
+
+        # checks if current time and time zone is accurately detected
+
+
+
+        for i in "The time is currently #{current_time} #{current_time_zone} here, correct?".chars.to_a
             print i
             sleep 0.05
         end
+
+
+        puts "\n"
+
+
+
+
+        # keeps user within the prompts until they give an answer
+
+        # if they give a y or yes, uppercase or lowercase or any of the letters mixed case, 'answered' is set to true, which exits the while loop, sets the first user's current time zone to whatever the current_time_zone variable is detecting(ex: CST, MT etc), and then answers with a thank you.
+
+        # else if they give a n or no, uppercase or lowercase or any of the letters mixed case, 'answered' is still set to true, but responds with asking what the current time zone is from the user. when user responds, time zone is collected manually from them and then answers with a thank you
+
+        # if user does not respond with any version of y/yes, or n/no, the default response asks user to answer with a yes or a no, waits for response, and will repeat until user gives a valid response
+
+
+
+        while answered_inner == false
+            confirm_or_deny_2 = gets.chomp
+            if (confirm_or_deny_2.upcase == "y".upcase || confirm_or_deny_2.upcase == "yes".upcase)
+                answered_inner = true
+                time_zone_users[:time_zone_user1][:time_zone] = current_time_zone
+                for i in "Yes? Thank you".chars.to_a
+                    print i
+                    sleep 0.03
+                end
+            
+            elsif (confirm_or_deny_2.upcase == "n".upcase || confirm_or_deny_2.upcase == "no".upcase)
+                answered_inner = true
+                for i in "No? Well, that's a problem....".chars.to_a
+                    print i
+                    sleep 0.05
+                end
+                sleep(1)
+                puts "\n"
+                for i in "What is your current time zone?".chars.to_a
+                    print i
+                    sleep 0.03
+                end
+                puts "\n"
+                manual_time_zone = gets.chomp
+                time_zone_users[:time_zone_user1][:time_zone] = manual_time_zone
+                for i in "#{manual_time_zone}? Okay, thank you".chars.to_a
+                    print i
+                    sleep 0.03
+                end
+            else
+                for i in "Yes or No please".chars.to_a
+                    print i
+                    sleep 0.03
+                end
+                puts "\n"
+            end
+
+        end
+
+
+
+
+        puts "\n"
+
+
+
+        # ask for user2 name
+
+        for i in "What is your associate's name?".chars.to_a
+            print i
+            sleep 0.03
+        end
+
+        puts "\n"
+
+        name_2 = gets.chomp
+
+
+
+        # store user input name value in the first hash
+
+
+
+        time_zone_users[:time_zone_user2][:name] = name_2
+
+        for i in "#{name_2}? Excellent, thank you!".chars.to_a
+            print i
+            sleep 0.03
+        end
+
         sleep(1)
+
         puts "\n"
-        for i in "What is your current time zone?".chars.to_a
+
+
+
+
+
+        # ask for user2 time zone
+
+
+
+        for i in "And what is #{name_2}'s time zone?".chars.to_a
             print i
             sleep 0.03
         end
+
         puts "\n"
-        manual_time_zone = gets.chomp
-        time_zone_users[:time_zone_user1][:time_zone] = manual_time_zone
-        for i in "#{manual_time_zone}? Okay, thank you".chars.to_a
+
+
+
+
+
+        user_2_time_zone = gets.chomp
+
+
+        # store user input name value in the first hash
+
+
+
+        time_zone_users[:time_zone_user2][:time_zone] = user_2_time_zone
+
+        for i in "#{user_2_time_zone}? Okay, thank you!".chars.to_a
             print i
             sleep 0.03
         end
+
+        sleep(1)
+
+        puts "\n"
+
+
+
     else
         for i in "Yes or No please".chars.to_a
             print i
@@ -183,32 +335,9 @@ while answered == false
 
 end
 
-
-
-
-
-
 puts "\n"
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-# answer = 1, 2
-# answer = "n/a"
-# time_zone_users[:time_zone_user1][:time_zone] = current_time_zone
-# time_zone_users[:time_zone_user1][:name] = gets.chomp
-# time_zone_users[:time_zone_user1][:availability][:monday][:time_slot_1] = answer
 
 
 
@@ -221,7 +350,7 @@ puts "\n"
 # on start of program, greets and prompts for username input - done
 # detects current time zone, asks user to verify if accurate - done
 # asks what current availability is weekly, including what days of week and hours of the day they ARE available, hours of the day they ARE NOT available, maybe store in a hash? - done
-# ask what other time zone you want to schedule with
+# ask what other time zone you want to schedule with - done
 # display availability in the other time zone
 
 
