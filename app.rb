ec8b506afbb97c7650fad5dd46295d9779e37831
JamesSalcedo <jamezzzsalcedo17@gmail.com>
2025-02-22 00:25:15 -0600

    Add comments to code for organization and for readability

--- app.rb
+++ app.rb
@@ -2,6 +2,9 @@ require "tzinfo"
 require "date"
 
 
+# variable initialization
+
+
 time = Time.new
 
 current_time = time.strftime("%I:%M")
@@ -10,6 +13,9 @@ current_time_zone = Time.now.zone
 
 answered = false
 
+
+
+# hashes with two hashes nested inside to store user input from user1 and user2
 
 
 time_zone_users = {
@@ -71,6 +77,8 @@ time_zone_users = {
 
 
 
+# greeting
+
 
 for i in "Hello, Welcome to Time Zone Converter!".chars.to_a
   print i
@@ -81,6 +89,10 @@ sleep(2)
 
 puts "\n"
 
+
+
+# ask for user1 name
+
 for i in "What is your name?".chars.to_a
     print i
     sleep 0.03
@@ -89,6 +101,12 @@ end
 puts "\n"
 
 name = gets.chomp
+
+
+
+# store user input name value in the first hash
+
+
 
 time_zone_users[:time_zone_user1][:name] = name
 
@@ -103,6 +121,10 @@ puts "\n"
 
 
 
+# checks if current time and time zone is accurately detected
+
+
+
 for i in "The time is currently #{current_time} #{current_time_zone} here, correct?".chars.to_a
     print i
     sleep 0.05
@@ -113,6 +135,14 @@ puts "\n"
 
 
 
+
+# keeps user within the prompts until they give an answer
+
+# if they give a y or yes, uppercase or lowercase or any of the letters mixed case, 'answered' is set to true, which exits the while loop, sets the first user's current time zone to whatever the current_time_zone variable is detecting(ex: CST, MT etc), and then answers with a thank you.
+
+# else if they give a n or no, uppercase or lowercase or any of the letters mixed case, 'answered' is still set to true, but responds with asking what the current time zone is from the user. when user responds, time zone is collected manually from them and then answers with a thank you
+
+# if user does not respond with any version of y/yes, or n/no, the default response asks user to answer with a yes or a no, waits for response, and will repeat until user gives a valid response
 
 while answered == false
     confirm_or_deny = gets.chomp
